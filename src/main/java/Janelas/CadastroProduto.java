e9b39f158894faa22df9c84d5231a5489962bce8
Luis Felipe <luisfelipemathiasleite@gmail.com>
2025-10-20 22:29:16 -0300

    Inicio ProdutoTableModel
    Inicio da classe ProdutoTableModel

--- src/main/java/Janelas/CadastroProduto.java
+++ src/main/java/Janelas/CadastroProduto.java
@@ -5,24 +5,21 @@
 package Janelas;
 
 import BD.Conexao;
+import Model.ProdutoTableModel;
 
 /**
  *
  * @author luis.fmleite
  */
 public class CadastroProduto extends javax.swing.JFrame {
+    ProdutoTableModel modelo = new ProdutoTableModel();
 
     /**
      * Creates new form CadastroProduto
      */
     public CadastroProduto() {
         initComponents();
-        
-        if (Conexao.getConnection() !=null){
-            System.out.println("Conexção realizada");
-        }else{
-            System.out.println("Falha na conexão");
-        }
+        jTProdutos.setModel(modelo);
     }
 
     /**
@@ -48,7 +45,7 @@ public class CadastroProduto extends javax.swing.JFrame {
         jBAlterar = new javax.swing.JButton();
         jBExcluir = new javax.swing.JButton();
         jScrollPane2 = new javax.swing.JScrollPane();
-        jTTabela = new javax.swing.JTable();
+        jTProdutos = new javax.swing.JTable();
 
         jTable1.setModel(new javax.swing.table.DefaultTableModel(
             new Object [][] {
@@ -142,7 +139,7 @@ public class CadastroProduto extends javax.swing.JFrame {
                 .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
 
-        jTTabela.setModel(new javax.swing.table.DefaultTableModel(
+        jTProdutos.setModel(new javax.swing.table.DefaultTableModel(
             new Object [][] {
                 {null, null, null, null},
                 {null, null, null, null},
@@ -153,7 +150,7 @@ public class CadastroProduto extends javax.swing.JFrame {
                 "Title 1", "Title 2", "Title 3", "Title 4"
             }
         ));
-        jScrollPane2.setViewportView(jTTabela);
+        jScrollPane2.setViewportView(jTProdutos);
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
@@ -233,8 +230,8 @@ public class CadastroProduto extends javax.swing.JFrame {
     private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JScrollPane jScrollPane2;
     private javax.swing.JTextField jTDescricao;
+    private javax.swing.JTable jTProdutos;
     private javax.swing.JTextField jTQuantidade;
-    private javax.swing.JTable jTTabela;
     private javax.swing.JTextField jTValor;
     private javax.swing.JTable jTable1;
     // End of variables declaration//GEN-END:variables
